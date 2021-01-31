49b0d9ed46a6700969704e075bf5f78007c06d6a
UncleAyo <40441673+UncleAyo@users.noreply.github.com>
2021-01-31 20:19:10 +0100

    Learning C

---
+++ C/helloWorld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Call me!\n");
+    return 0;
+}
---
+++ C/variables.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int a=5, b=4, c=10,d=13;
+    a=b+c-a+d;
+    printf("%d",a);
+    return 0;
+}
