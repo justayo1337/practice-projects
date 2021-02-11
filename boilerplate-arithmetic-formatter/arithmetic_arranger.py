2bba002727d7d7d5ae505e233efbb742a86cf68a
UncleAyo <40441673+UncleAyo@users.noreply.github.com>
2021-02-11 18:32:26 +0100

    new commit

--- boilerplate-arithmetic-formatter/arithmetic_arranger.py
+++ boilerplate-arithmetic-formatter/arithmetic_arranger.py
@@ -7,10 +7,10 @@ def arithmetic_arranger(problems,*args):
             changedList.append(a)
             #arranged_problems=0
         currentPosition=0
-        while currentPosition!=3:
+        while currentPosition!=4:
             value=""
-            spacingMain="     "
-            
+            spacingMain="   "
+            lines="------      "
             for i in range(Lproblems):
                 variableSpacing=" "
                 if currentPosition==1:
@@ -29,18 +29,36 @@ def arithmetic_arranger(problems,*args):
                     elif (len(changedList[i][0])-len(changedList[i][2]))==-3:
                         variableSpacing*=5
                     if i>0:
-                            value+=spacingMain+ changedList[i][currentPosition] +variableSpacing+ changedList[i][currentPosition+1]
+                            value+="     "+ changedList[i][currentPosition] +variableSpacing+ changedList[i][currentPosition+1]
                     else:
                         value+=changedList[i][currentPosition] + spacingMain + changedList[i][currentPosition+1]
                     
                 elif currentPosition==0:
-                    value+="     "+changedList[i][currentPosition]+variableSpacing
+                    value+=spacingMain+changedList[i][currentPosition]+spacingMain
+                elif currentPosition==2:
+                    value+=lines
+                elif currentPosition==3:
+                    addOrs=0
+                    if changedList[i][1]=="+":
+                        addOrs=str(add(int(changedList[i][0]),int(changedList[i][2])))
+                    elif changedList[i][1]=="-":
+                        addOrs=str(subtract(int(changedList[i][0]),int(changedList[i][2])))
+                    if i>0:
+                        value+=spacingMain*2+addOrs
+                    else:
+                        value+=spacingMain+addOrs
+
+
             currentPosition+=1
             print(value)
     
         print(changedList)
     
-    
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
 
 
 
