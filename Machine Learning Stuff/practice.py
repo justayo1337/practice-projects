e5d35ef60eb7f605b245b08172c10d1196cdf1d3
UncleAyo <aa4449@drexel.edu>
2021-04-15 07:28:55 +0100

    Learning Data Science

--- Machine Learning Stuff/practice.py
+++ Machine Learning Stuff/practice.py
@@ -15,7 +15,7 @@ for i,c in enumerate(celsius_q):
 l0 = tf.keras.layers.Dense(units=1 , input_shape=[1])
 model= tf.keras.Sequential([l0])
 model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
-history=model.fit(celsius_q,fahrenheit_a,epochs=5000,verbose=False)
+history=model.fit(celsius_q,fahrenheit_a,epochs=500,verbose=False)
 print("Finished training the model")
 
 
