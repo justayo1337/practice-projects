0a8438ccfdcd938d6530c99933a36173d4f97b94
UncleAyo <aa4449@drexel.edu>
2021-05-10 07:29:51 +0100

    Learning some more C

--- C/hello.c
+++ C/hello.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(void) {
-printf("Hello World!");
 
-}
-
-int test(){
-    
+int main(){
+    /* my first program in C */
+    printf("Hello World");
+    return 0;
 }
