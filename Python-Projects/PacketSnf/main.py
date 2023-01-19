5de5eb92ce45114eba5b326888b52a6e4372c3a8
justayo1337 <ayoto.adetunji@gmail.com>
2023-01-19 04:51:43 -0500

    Simple Sniffer

--- Python-Projects/PacketSnf/main.py
+++ Python-Projects/PacketSnf/main.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python3
 import socket as soc
 
 
 #setup socket for windows
-s = soc.socket(soc.AF_INET,soc.SOCK_RAW,soc.IPPROTO_IP)
-s.bind(("172.12.0.161",1337))
-s.setsockopt(soc.IPPROTO_IP, soc.IP_HDRINCL,1)
-s.ioctl(soc.SIO_RCVALL, soc.RCVALL_ON)
+#s = soc.socket(soc.AF_INET,soc.SOCK_RAW,soc.IPPROTO_IP)
+s = soc.socket(soc.PF_PACKET,soc.SOCK_RAW,soc.ntohs(0x0800))
+
+#s.bind(("172.28.66.16",1337))
+#s.setsockopt(soc.IPPROTO_IP, soc.IP_HDRINCL,1)
+#s.ioctl(soc.SIO_RCVALL, soc.RCVALL_ON)
 
 
 while True:
-
+    print("Waiting on packetssss...........")
     print(s.recvfrom(65565))
